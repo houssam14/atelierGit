65f2cc97d525a4eda4c7723484160e4fbc8d8baf
houssam14 <adkinshoussam14@gmail.com>
2019-04-24 09:41:05 +0100

    first Commit

--- app/src/main/java/com/spirit/force/ateliergit/MainActivity.java
+++ app/src/main/java/com/spirit/force/ateliergit/MainActivity.java
@@ -2,9 +2,10 @@ package com.spirit.force.ateliergit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
